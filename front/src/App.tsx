c4e102130a2de699bab40ba8d8b7a52a6950a42e
Akamig <akamig@posteo.net>
2022-04-03 20:17:12 +0900

    Solved Scroll to bottom issue

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,29 +1,52 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import useSWR from "swr";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("me");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages((event) => messages.concat(message));
+    setMessage("");
+  };
 
   return (
     <div className="App">
       <div className="Main">
         <div className="MessageWrapper">
           <div className="TopContent">
-          <img src={logo} className="logo" alt="logo" />
-          <span className="TopHeader">COVALENTER</span>
-            </div>
+            <img src={logo} className="logo" alt="logo" />
+            <span className="TopHeader">COVALENTER</span>
+          </div>
           <div className="ScrollContent">
             <ol className="ScrollInner">
-              <li>AB: Hi</li>
+              {messages.map((message, i) => (
+                <li key={i}>
+                  {name}: {message}
+                </li>
+              ))}
             </ol>
           </div>
-          <form className="Input" action="">
-            <div className="InputField">
-              <input type="text" name="WhatsHappening" id="textput" />
-            </div>
-            <div className="Button Wrapper">
-              <input type="button" value="" />
-            </div>
+          <form className="Input">
+            <input
+              className="text"
+              placeholder="메시지를 입력하슈."
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+              onKeyPress={(event) =>
+                event.key === "Enter" ? sendMessage(event) : null
+              }
+            />
+            <button
+              className="sendButton"
+              onClick={(event) => sendMessage(event)}
+            >
+              보내기
+            </button>
           </form>
         </div>
       </div>
